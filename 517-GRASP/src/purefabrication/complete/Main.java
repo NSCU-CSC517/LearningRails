d73576b3aebd6466a1464e313b75b339ac3841e6
Brent Younce <bjyounce@ncsu.edu>
2018-07-19 10:32:10 -0400

    Re-name classes

--- 517-GRASP/src/purefabrication/complete/Main.java
+++ 517-GRASP/src/purefabrication/complete/Main.java
@@ -2,21 +2,21 @@ package purefabrication.complete;
 
 import java.util.Date;
 
+import purefabrication.incomplete.Event;
+import purefabrication.incomplete.EventStorage;
+
 public class Main {
 
 	public static void main(String[] args) {
 		//...
-		//1         //2
-		___ e1 = new ___(new Date(), "Server 1", "Turned on server");
-		EventStorage.___(e1); //3
+		Event e1 = new Event(new Date(), "Server 1", "Turned on server");
+		EventStorage.saveToDB(e1);
 		//...
-		//4            //5
-		___ e2 = new Event(new Date(), "Server 1", "Started webapp");
-		EventStorage.___(e2); //6
+		Event e2 = new Event(new Date(), "Server 1", "Started webapp");
+		EventStorage.saveToDB(e2);
 		//...
-		//7             //8
-		___ e3 = new ___(new Date(), "Server 1", "New connection!");
-		EventStorage.___(e3); //9
+		Event e3 = new Event(new Date(), "Server 1", "New connection!");
+		EventStorage.saveToDB(e3);
 		//...
 
 	}
--- 517-GRASP/src/purefabrication/incomplete/Main.java
+++ 517-GRASP/src/purefabrication/incomplete/Main.java
@@ -2,18 +2,24 @@ package purefabrication.incomplete;
 
 import java.util.Date;
 
+import purefabrication.complete.Event;
+import purefabrication.complete.EventStorage;
+
 public class Main {
 
 	public static void main(String[] args) {
 		//...
-		Event e1 = new Event(new Date(), "Server 1", "Turned on server");
-		EventStorage.saveToDB(e1);
+		//1         //2
+		___ e1 = new ___(new Date(), "Server 1", "Turned on server");
+		EventStorage.___(e1); //3
 		//...
-		Event e2 = new Event(new Date(), "Server 1", "Started webapp");
-		EventStorage.saveToDB(e2);
+		//4            //5
+		___ e2 = new Event(new Date(), "Server 1", "Started webapp");
+		EventStorage.___(e2); //6
 		//...
-		Event e3 = new Event(new Date(), "Server 1", "New connection!");
-		EventStorage.saveToDB(e3);
+		//7             //8
+		___ e3 = new ___(new Date(), "Server 1", "New connection!");
+		EventStorage.___(e3); //9
 		//...
 
 	}
