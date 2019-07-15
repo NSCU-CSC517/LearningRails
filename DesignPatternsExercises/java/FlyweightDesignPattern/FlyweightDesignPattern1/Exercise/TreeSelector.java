1804e49e9e2a3acef0785543907f24c18fa245e0
Nikolay Titov <nikolay.titov@nokia.com>
2019-07-15 12:19:44 -0400

    Improved Flyweight Design Pattern Exercise in Java:   - Reformatted files   - Set blanks to be filled in at more appropriated classes and methods   - Improved README.md file for exercise instructions

--- DesignPatternsExercises/java/FlyweightDesignPattern/FlyweightDesignPattern1/Exercise/TreeSelector.java
+++ DesignPatternsExercises/java/FlyweightDesignPattern/FlyweightDesignPattern1/Exercise/TreeSelector.java
@@ -2,21 +2,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TreeSelector {
-    private final static Map<String, Tree> treeBySpec = new HashMap<>();
+    private final static Map<String, Tree> treeMap = new HashMap<>();
 
     public static Tree getTree(String treeType) {
-        if (treeBySpec.containsKey(treeType))
-            return treeBySpec.get(treeType);
+        if (treeMap.containsKey(treeType))
+            return treeMap.get(treeType);
         else {
             Tree tree = null;
             if (treeType.equals("Oak Tree")) {
-                tree = new __________(8)__________ ();
-                treeBySpec.put(treeType, tree);
+                tree = __________(8)__________ ();
+                treeMap.put(treeType, tree);
             } else if (treeType.equals("Flowering Cherry")) {
-                tree = new __________(9)__________ ();
-                treeBySpec.put(treeType, tree);
+                tree = __________(9)__________ ();
+                treeMap.put(treeType, tree);
             } else {
-                System.out.println(treeType + " is not found");
+                throw new IllegalArgumentException(treeType + " is not found");
             }
             return tree;
         }
--- DesignPatternsExercises/java/FlyweightDesignPattern/FlyweightDesignPattern1/Solution/TreeSelector.java
+++ DesignPatternsExercises/java/FlyweightDesignPattern/FlyweightDesignPattern1/Solution/TreeSelector.java
@@ -2,23 +2,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TreeSelector {
-    private final static Map<String, Tree> treeBySpec = new HashMap<>();
+    private final static Map<String, Tree> treeMap = new HashMap<>();
 
     public static Tree getTree(String treeType) {
-        if (treeBySpec.containsKey(treeType))
-            return treeBySpec.get(treeType);
+        if (treeMap.containsKey(treeType))
+            return treeMap.get(treeType);
         else {
             Tree tree = null;
             if (treeType.equals("Oak Tree")) {
-                // tree = new __________(8)__________ ();
+                // tree = __________(8)__________ ();
                 tree = new OakTree();
-                treeBySpec.put(treeType, tree);
+                treeMap.put(treeType, tree);
             } else if (treeType.equals("Flowering Cherry")) {
-                // tree = new __________(9)__________ ();
+                // tree = __________(9)__________ ();
                 tree = new FloweringCherry();
-                treeBySpec.put(treeType, tree);
+                treeMap.put(treeType, tree);
             } else {
-                System.out.println(treeType + " is not found");
+                throw new IllegalArgumentException(treeType + " is not found");
             }
             return tree;
         }
